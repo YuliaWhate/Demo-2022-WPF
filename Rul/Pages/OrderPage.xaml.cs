1fa8802cf86f15590f97b23420a167d187fe2c68
Митина Юлия <yulia_mimi0@inbox.ru>
2022-07-22 19:29:25 +0300

    22.07.2022

--- Rul/Pages/OrderPage.xaml.cs
+++ Rul/Pages/OrderPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Rul.Entities;
 
 namespace Rul.Pages
@@ -28,11 +29,18 @@ namespace Rul.Pages
             DataContext = this;
             productList = products;
             lViewOrder.ItemsSource = productList;
-            cmbPickupPoint.ItemsSource = RulEntities.GetContext().PickupPoint.Select(p => p.Address).ToList();
 
+            cmbPickupPoint.ItemsSource = RulEntities.GetContext().PickupPoint.ToList();          
+
+            
 
             if (user != null)
                 txtUser.Text = user.UserSurname.ToString() + user.UserName.ToString() + " " + user.UserPatronymic.ToString();
+        }
+
+        public void UpdateData(object sender, object e)
+        {
+            lViewOrder.ItemsSource = productList;
         }
 
         public string Total
@@ -45,12 +53,51 @@ namespace Rul.Pages
         }
         private void btnOrderSave_Click(object sender, RoutedEventArgs e)
         {
+            var productArticle = productList.Select(p => p.ProductArticleNumber).ToArray();
+            Random random = new Random();
+            try
+            {
+                Order newOrder = new Order()
+                {
+                    OrderStatus = "Новый",
+                    OrderDate = DateTime.Now,
+                    OrderPickupPoint = 1,
+                    OrderDeliveryDate = DateTime.Now,
+                    ReceiptCode = random.Next(100, 1000),
+                    ClientFullName = txtUser.Text,
+                   
+                };
+                RulEntities.GetContext().Order.Add(newOrder);
 
+                for (int i = 0; i < productArticle.Count(); i++)
+                {
+                    OrderProduct newOrderProduct = new OrderProduct()
+                    {
+                        OrderID = newOrder.OrderID,
+                        ProductArticleNumber = productArticle[i],
+                        ProductCount = 1
+                    };
+                    RulEntities.GetContext().OrderProduct.Add(newOrderProduct);
+                }
+                
+                RulEntities.GetContext().SaveChanges();
+                MessageBox.Show("Данные успешно добавлены!");
+                NavigationService.Navigate(new OrderTicketPage());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
 
         private void btnDeleteProduct_Click(object sender, RoutedEventArgs e)
         {
             productList.Remove(lViewOrder.SelectedItem as Product);
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateData;
+            timer.Start();
         }
     }
 }
