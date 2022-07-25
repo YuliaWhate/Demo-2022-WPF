4f4c6c75e5b0e3728ac89da5f05e997f07eaf7e7
Митина Юлия <yulia_mimi0@inbox.ru>
2022-07-25 09:25:05 +0300

    25.07.2022

--- Rul/Pages/OrderTicketPage.xaml.cs
+++ Rul/Pages/OrderTicketPage.xaml.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Xps.Packaging;
 using Microsoft.Win32;
 using Rul.Entities;
-using Rul.Pages;
 using System.Windows.Xps;
 using System.IO;
 
